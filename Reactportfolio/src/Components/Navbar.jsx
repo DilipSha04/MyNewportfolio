1525ae410b3a9f16c999565d69d5a614b61c3771
DilipSha04 <imdilip.004@gmail.com>
2024-01-22 20:13:34 +0530

    update social media link

--- Reactportfolio/src/Components/Navbar.jsx
+++ Reactportfolio/src/Components/Navbar.jsx
@@ -10,17 +10,17 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Portfolio</motion.span>
         <div className="social">
-          <a href="">
+          <a href="https://github.com/DilipSha04">
             <img
               className="social-img"
               src="/img/github (2).png"
               alt=""
             />
           </a>
-          <a href="">
+          <a href="https://linkedin.com/in/dilipsha04">
             <img className="social-img" src="/img/linkedin.png" alt="" />
           </a>
-          <a href="">
+          <a href="https://twitter.com/imdilip_04">
             <img className="social-img" src="/img/twitter.png" alt="" />
           </a>
         </div>
